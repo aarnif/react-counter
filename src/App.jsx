2adf0e61323af5f4adc2d4dfbed109f7243a4268
aarnif <aarni.fagerstrom@gmail.com>
2023-10-30 14:01:48 +0200

    Lift Counter functionality to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,61 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import { db } from "./FireBase";
+import { collection, getDocs, doc, updateDoc } from "firebase/firestore/lite";
 import Counter from "./components/Counter";
 
 function App() {
-  return <Counter />;
+  const [count, setCount] = useState(null);
+
+  useEffect(() => {
+    // Fetch data from database when page first loads
+    const getCount = async (db) => {
+      const savedCountSnapShot = await getDocs(collection(db, "savedCount"));
+      return savedCountSnapShot.docs[0].data();
+    };
+    getCount(db).then((data) => {
+      setCount(data.value);
+      console.log("Previous count value fetched!");
+    });
+  }, []);
+
+  useEffect(() => {
+    const updateData = async (db) => {
+      const updateData = await updateDoc(doc(db, "savedCount", "countValue"), {
+        value: count,
+      });
+      return updateData;
+    };
+
+    // Update database only after 500ms has passed from previous click or when page first loads
+    const updateDataTimeout = setTimeout(() => {
+      updateData(db).then(() => {
+        console.log("Count value saved to database!");
+      });
+    }, 500);
+
+    return () => clearTimeout(updateDataTimeout);
+  }, [count]);
+
+  const increaseCount = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const decreaseCount = () => {
+    setCount((prevCount) => (prevCount > 0 ? prevCount - 1 : 0));
+  };
+
+  const resetCount = () => {
+    setCount(0);
+  };
+  return (
+    <Counter
+      count={count}
+      increaseCount={increaseCount}
+      decreaseCount={decreaseCount}
+      resetCount={resetCount}
+    />
+  );
 }
 
 export default App;
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,55 +1,8 @@
 import "./Counter.css";
-import { useEffect, useState } from "react";
-import { db } from "../FireBase";
-import { collection, getDocs, doc, updateDoc } from "firebase/firestore/lite";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 
-const Counter = () => {
-  const [count, setCount] = useState(null);
-
-  useEffect(() => {
-    // Fetch data from database when page first loads
-    const getCount = async (db) => {
-      const savedCountSnapShot = await getDocs(collection(db, "savedCount"));
-      return savedCountSnapShot.docs[0].data();
-    };
-    getCount(db).then((data) => {
-      setCount(data.value);
-      console.log("Previous count value fetched!");
-    });
-  }, []);
-
-  useEffect(() => {
-    const updateData = async (db) => {
-      const updateData = await updateDoc(doc(db, "savedCount", "countValue"), {
-        value: count,
-      });
-      return updateData;
-    };
-
-    // Update database only after 500ms has passed from previous click or when page first loads
-    const updateDataTimeout = setTimeout(() => {
-      updateData(db).then(() => {
-        console.log("Count value saved to database!");
-      });
-    }, 500);
-
-    return () => clearTimeout(updateDataTimeout);
-  }, [count]);
-
-  const increaseCount = () => {
-    setCount((prevCount) => prevCount + 1);
-  };
-
-  const decreaseCount = () => {
-    setCount((prevCount) => (prevCount > 0 ? prevCount - 1 : 0));
-  };
-
-  const resetCount = () => {
-    setCount(0);
-  };
-
+const Counter = ({ count, increaseCount, decreaseCount, resetCount }) => {
   return (
     <main className="main-content">
       <h1 className="count">
